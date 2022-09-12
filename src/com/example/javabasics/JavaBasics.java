a52770a1c6cbbb26888b4a4d752b94e321f60a5a
Edmundas PC <edmundaskaminskas@live.com>
2022-09-13 01:53:48 +0300

    Initial Commit

--- src/com/example/javabasics/JavaBasics.java
+++ src/com/example/javabasics/JavaBasics.java
@@ -1,9 +1,16 @@
 package com.example.javabasics;
 
+// Pirma užduotis:
 public class JavaBasics {
     public static void main(String[] args) {
-        System.out.println("Push!");
-        System.out.println("Push2!");
+        /*
+            Antra užduotis:
+             Edmundas Kaminskas PRIF20/3 20184342
+         */
+        String vardas="Edmundas";
+        String pavarde="Kaminskas";
+        int n=vardas.length();
+        int m=pavarde.length();
     }
 
 }
