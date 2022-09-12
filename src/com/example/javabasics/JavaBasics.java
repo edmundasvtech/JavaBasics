903bcfcfe40a8b522874ef445de6e3c3b7b46e1f
Edmundas Laptop <hao@inbox.lt>
2022-09-13 01:12:58 +0300

    !!!

--- src/com/example/javabasics/JavaBasics.java
+++ src/com/example/javabasics/JavaBasics.java
@@ -4,6 +4,13 @@ public class JavaBasics {
     public static void main(String[] args) {
         System.out.println("Push!");
         System.out.println("Push2!");
+        System.out.println("Push2!");
+        System.out.println("Push2!");
+        System.out.println("Push2!");
+        System.out.println("Push2!");
+        System.out.println("Push2!");
+        System.out.println("Push2!");
+        System.out.println("Push2!");
     }
 
 }
