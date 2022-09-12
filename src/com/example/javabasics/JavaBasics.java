d425bf6deafefd823be4d52798302484225a5f19
edmundasvtech <edmundas-paulius.kaminskas@stud.vilniustech.lt>
2022-09-13 01:29:31 +0300

    1

--- src/com/example/javabasics/JavaBasics.java
+++ src/com/example/javabasics/JavaBasics.java
@@ -3,6 +3,7 @@ package com.example.javabasics;
 public class JavaBasics {
     public static void main(String[] args) {
         System.out.println("Push!");
+        System.out.println("Push!");
     }
 
 }
