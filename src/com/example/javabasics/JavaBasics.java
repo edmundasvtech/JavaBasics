27d326629209a10aa1783a652998ac50500dccc4
edmundasvtech <edmundas-paulius.kaminskas@stud.vilniustech.lt>
2022-09-13 01:17:37 +0300

    1

--- src/com/example/javabasics/JavaBasics.java
+++ src/com/example/javabasics/JavaBasics.java
@@ -3,14 +3,6 @@ package com.example.javabasics;
 public class JavaBasics {
     public static void main(String[] args) {
         System.out.println("Push!");
-        System.out.println("Push2!");
-        System.out.println("Push2!");
-        System.out.println("Push2!");
-        System.out.println("Push2!");
-        System.out.println("Push2!");
-        System.out.println("Push2!");
-        System.out.println("Push2!");
-        System.out.println("Push2!");
     }
 
 }
